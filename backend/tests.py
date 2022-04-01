5c43127b1ac6d2938f7945e0261d058eefb65357
lorossi <mail@lorenzoros.si>
2022-04-01 09:27:29 +0200

    added lots of tests

--- backend/tests.py
+++ backend/tests.py
@@ -1,5 +1,17 @@
 import unittest
-from database import Database, DuplicatedIdException
+
+from string import ascii_uppercase
+from random import choices, randint, random
+
+from database import Database, DuplicatedIdException, InvalidQueryValueException
+
+
+def random_string(k=16):
+    return "".join(choices(ascii_uppercase, k=k))
+
+
+def random_between(a, b):
+    return random() * (b - a) + 1
 
 
 class TestDB(unittest.TestCase):
@@ -14,6 +26,11 @@ class TestDB(unittest.TestCase):
     def test_drop(self):
         d = Database("test_settings.toml")
         d.dropDatabase()
+        self.assertEqual(len(d.listMachines()), 0)
+        self.assertEqual(len(d.listMachineTypes()), 0)
+        self.assertEqual(len(d.listMaintenances()), 0)
+        self.assertEqual(len(d.listMachines()), 0)
+        self.assertEqual(len(d.listUsers()), 0)
 
     def test_simple_add_roles(self):
         d = self.initDatabase()
@@ -101,6 +118,7 @@ class TestDB(unittest.TestCase):
             "Colombo",
             "Fumagalli",
         ]
+
         # just to make sure
         self.assertEqual(len(names), len(surnames))
 
@@ -134,7 +152,7 @@ class TestDB(unittest.TestCase):
         self.assertEqual(p.user_id, u.user_id)
 
         # set a user card
-        UUID = "AAAAAAAAA-BBBBBBBBBBB-C"
+        UUID = random_string()
         d.setUserCardUUID(u.user_id, UUID)
         self.assertEqual(UUID, d.getUserCardUUID(u.user_id))
 
@@ -150,7 +168,131 @@ class TestDB(unittest.TestCase):
 
     def test_simple_add_machines(self):
         d = self.initDatabase()
-        # TODO
+
+        TYPES = 10
+        MACHINES = 10
+        current_id = 0
+
+        # create machines for each type
+        for i in range(TYPES):
+            NAME = random_string(10)
+            d.addMachineType(type_id=i, type_name=NAME)
+            self.assertEqual(len(d.listMachineTypes()), i + 1)
+            for _ in range(MACHINES):
+                name = random_string(6)
+                d.addMachine(current_id, name, i)
+                current_id += 1
+
+        # test that they have been added
+        self.assertEqual(MACHINES * TYPES, len(d.listMachines()))
+
+        for current_id in range(MACHINES * TYPES):
+            # test rename
+            NAME = random_string(10)
+            d.setMachineName(current_id, NAME)
+            self.assertEqual(d.getMachineName(current_id), NAME)
+            # test new type
+            NEW_TYPE = randint(0, TYPES - 1)
+            d.setMachineMachineType(current_id, NEW_TYPE)
+            self.assertEqual(NEW_TYPE, d.getMachineMachineType(current_id))
+
+            d.removeMachine(current_id)
+            # test delete
+            self.assertEqual(MACHINES * TYPES - current_id - 1, len(d.listMachines()))
+
+        # test delete
+        self.assertEqual(0, len(d.listMachines()))
+
+    def test_simple_maintenance(self):
+        d = self.initDatabase()
+
+        MAINTENANCES = 100
+
+        for x in range(MAINTENANCES):
+            hours_between = random_between(5, 10)
+            description = random_string(50)
+            new_id = d.addMaintenance(hours_between, description)
+            # check if the new id is the same as the index
+            self.assertEqual(x, new_id)
+            # check getters and setters
+            self.assertEqual(d.getMaintenanceHoursBetween(new_id), hours_between)
+            self.assertEqual(d.getMaintenanceDescription(new_id), description)
+
+            hours_between = random_between(5, 10)
+            description = random_string(50)
+            d.setMaintenanceHoursBetween(x, hours_between)
+            d.setMaintenanceDescription(x, description)
+            self.assertEqual(d.getMaintenanceHoursBetween(new_id), hours_between)
+            self.assertEqual(d.getMaintenanceDescription(new_id), description)
+
+        # check removal
+        for x in range(MAINTENANCES):
+            d.removeMaintenance(x)
+            self.assertEqual(MAINTENANCES - x - 1, len(d.listMaintenances()))
+            d.removeMaintenance(x)
+            self.assertEqual(MAINTENANCES - x - 1, len(d.listMaintenances()))
+
+        self.assertEqual(len(d.listMaintenances()), 0)
+
+        # check double add
+        d.addMaintenance(maintenance_id=0, description="WHAT", hours_between=10)
+        with self.assertRaises(DuplicatedIdException):
+            d.addMaintenance(
+                maintenance_id=0, description="YOU AGAIN", hours_between=10
+            )
+
+    def test_machine_maintenance_interaction(self):
+        d = self.initDatabase()
+
+        MACHINE_TYPE = "drill"
+        MACHINES = 3
+        # create simple machine types
+        d.addMachineType(type_id=0, type_name=MACHINE_TYPE)
+
+        # create simple machine
+        for x in range(MACHINES):
+            d.addMachine(x, f"TEST{x}", 0, 0)
+
+        # create simple maintenances
+        MAINTENANCES_DESC = ["change oil", "clean mirror", "empty bin"]
+        for i, m in enumerate(MAINTENANCES_DESC):
+            d.addMaintenance(maintenance_id=i, description=m, hours_between=10)
+            d.addMachineMaintenance(i, i)
+            self.assertEqual(len(d.getMachineMaintenances(i)), 1)
+            self.assertEqual(d.getMachineMaintenances(i)[0].maintenance_id, i)
+            self.assertEqual(
+                d.getMachineMaintenances(i)[0].description, MAINTENANCES_DESC[i]
+            )
+
+        # remove maintenances from machines
+        for x in range(MACHINES):
+            d.removeMachineMaintenance(x, x)
+            self.assertEqual(len(d.getMachineMaintenances(x)), 0)
+
+        # remove maintenances
+        for i in range(len(MAINTENANCES_DESC)):
+            d.removeMaintenance(i)
+            self.assertEqual(len(d.listMaintenances()), len(MAINTENANCES_DESC) - i - 1)
+        # check that everything was removed
+        self.assertEqual(len(d.listMaintenances()), 0)
+
+    def test_missing_query(self):
+        d = self.initDatabase()
+
+        with self.assertRaises(InvalidQueryValueException):
+            d.getUser(user_id=0)
+
+        with self.assertRaises(InvalidQueryValueException):
+            d.getMachineType(type_id=0)
+
+        with self.assertRaises(InvalidQueryValueException):
+            d.getRole(role_id=0)
+
+        with self.assertRaises(InvalidQueryValueException):
+            d.getMaintenance(maintenance_id=0)
+
+        with self.assertRaises(InvalidQueryValueException):
+            d.getMachine(machine_id=0)
 
 
 if __name__ == "__main__":
